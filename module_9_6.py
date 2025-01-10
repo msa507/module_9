6d448b3ffb2af845deba94c652d88b815801e2c3
msa507 <msa10101962@gmail.com>
2025-01-10 11:54:06 +0500

    Add files via upload

---
+++ module_9_6.py
@@ -0,0 +1,17 @@
+def all_variants(text):
+    if not isinstance(text, str):
+        raise TypeError('Аргумент должен быть строкой')
+
+    n = len(text)
+
+    for length in range(1, n + 1):
+        for start in range(n - length + 1):
+            end = start + length
+            subsequence = text[start:end]
+            yield subsequence
+
+
+# Пример использования функции
+a = all_variants("abc")
+for i in a:
+    print(i)
